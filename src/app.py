5289ab86bfdb32fcd944a35b2d6f5846339a6ddc
gwt805 <1973735972@qq.com>
2024-03-18 19:53:44 +0800

    每个表格都将展示4列; 文件写入异常提示报错修复

--- src/app.py
+++ src/app.py
@@ -17,6 +17,7 @@ class Fun(Mainui):
         self.setupUi()
         self.clean_result_uuid_list = []
         self.send_rcc_uuid_list = []
+        self.base_img_label = {"large_garbage": "垃圾", "garbage": "垃圾", "sewage": "脏污"}
         self.same_list = []
         self.csv_data = {}
         self.show()
@@ -38,7 +39,7 @@ class Fun(Mainui):
         file = QFileDialog.getOpenFileName(self, "选择 info.csv 文件", filter="Text Files (*.csv)")
         if file is not None:
             self.input_csv.setText(file[0])
-            QTimer.singleShot(1000, self.export_csv_img)
+            QTimer.singleShot(1000, self.read_csv_img)
         self.listen_path()
 
     def load_img_files(self):
@@ -48,7 +49,7 @@ class Fun(Mainui):
 
         self.listen_path()
     
-    def export_csv_img(self):
+    def read_csv_img(self):
         data = {}
         try:
             with open(self.input_csv.text(), 'r') as f:
@@ -58,11 +59,11 @@ class Fun(Mainui):
 
             for row in reader:
                 image_name = row[1]
-                # class_name = row[2]
+                class_name = row[2]
                 uuid = row[4]
 
                 if image_name in imglist:
-                    data[uuid] = image_name
+                    data[uuid] = {"img_name": image_name, "class_image": class_name}
             self.csv_data = data
             if self.input_log.text() == "":
                 reply = QMessageBox(QMessageBox.Question, self.tr("提示"), self.tr("info.csv 已加载完成, 请加载log文件 !"), QMessageBox.NoButton, self)
@@ -126,17 +127,26 @@ class Fun(Mainui):
     def create_table(self):
         length_clean_result = len(self.clean_result_uuid_list)
         model = QStandardItemModel(self.tableView)
-        model.setHorizontalHeaderLabels(['UUID', '图片名'])
-        model.setColumnCount(2)
+        model.setHorizontalHeaderLabels(['UUID', '标签', '中文标签', '图片名'])
+        model.setColumnCount(4)
         model.setRowCount(length_clean_result)
         for i in range(length_clean_result):
-            item = QStandardItem(self.clean_result_uuid_list[i])
+            item1 = QStandardItem(self.clean_result_uuid_list[i])
             if self.clean_result_uuid_list[i] in self.csv_data:
-                item1 = QStandardItem(self.csv_data[self.clean_result_uuid_list[i]])
+                tmp_dict = edict(self.csv_data[self.clean_result_uuid_list[i]])
+                self.clean_result_uuid_list[i] = [self.clean_result_uuid_list[i], tmp_dict.class_name, self.base_img_label[tmp_dict.class_name], tmp_dict.img_name]
+                item2 = QStandardItem(tmp_dict.class_name)
+                item3 = QStandardItem(self.base_img_label[tmp_dict.class_name])
+                item4 = QStandardItem(tmp_dict.img_name)
             else:
-                item1 = QStandardItem("")
-            model.setItem(i, 0, item)
-            model.setItem(i, 1, item1)
+                self.clean_result_uuid_list[i] = (self.clean_result_uuid_list[i], "", "", "")
+                item2 = QStandardItem("")
+                item3 = QStandardItem("")
+                item4 = QStandardItem("")
+            model.setItem(i, 0, item1)
+            model.setItem(i, 1, item2)
+            model.setItem(i, 2, item3)
+            model.setItem(i, 3, item4)
         self.tableView.setModel(model)
         self.tableView.setColumnWidth(0, 300)
         self.tableView.setEditTriggers(QAbstractItemView.NoEditTriggers)
@@ -152,17 +162,20 @@ class Fun(Mainui):
         
         for i in range(length_send_rcc):
             tmp = self.send_rcc_uuid_list[i]
-            if tmp[-1] == "large_garbage" or tmp[-1] == "garbage":
-                tmp.append("垃圾")
-            if tmp[-1] == "sewage":
-                tmp.append("脏污")
+            tmp.append(self.base_img_label[tmp[-1]])
+
             item1 = QStandardItem(tmp[0])
             item2 = QStandardItem(tmp[1])
             item3 = QStandardItem(tmp[2])
             if tmp[0] in self.csv_data:
-                item4 = QStandardItem(self.csv_data[tmp[0]])
+                tmp.append(self.csv_data[tmp[0]]['img_name'])
+                item4 = QStandardItem(self.csv_data[tmp[0]]['img_name'])
             else:
+                tmp.append("")
                 item4 = QStandardItem("")
+
+            self.send_rcc_uuid_list[i] = tmp
+
             model_2.setItem(i, 0, item1)
             model_2.setItem(i, 1, item2)
             model_2.setItem(i, 2, item3)
@@ -174,18 +187,29 @@ class Fun(Mainui):
         # ===================================================================
         length_same = len(self.same_list)
         model_3 = QStandardItemModel(self.tableView_3)
-        model_3.setColumnCount(2)
+        model_3.setColumnCount(4)
         model_3.setRowCount(length_same)
-        model_3.setHorizontalHeaderLabels(['UUID', '图片名'])
+        model_3.setHorizontalHeaderLabels(['UUID', '标签', '中文标签', '图片名'])
         
         for i in range(length_same):
-            item = QStandardItem(self.same_list[i])
+            item1 = QStandardItem(self.same_list[i])
             if self.clean_result_uuid_list[i] in self.csv_data:
-                item1 = QStandardItem(self.csv_data[self.clean_result_uuid_list[i]])
+                dict_tmp = edict(self.csv_data[self.same_list[i]])
+                self.same_list[i] = [self.same_list[i], dict_tmp.class_name, self.base_img_label[dict_tmp.class_name], self.base_img_label[dict_tmp.img_name]]
+                item2 = QStandardItem(dict_tmp.class_name)
+                item3 = QStandardItem(self.base_img_label[dict_tmp.class_name])
+                item4 = QStandardItem(self.base_img_label[dict_tmp.img_name])
             else:
-                item1 = QStandardItem("")
-            model_3.setItem(i, 0, item)
-            model_3.setItem(i, 1, item1)
+                self.same_list[i] = [self.same_list[i], "", "", ""], 
+                item2 = QStandardItem("")
+                item3 = QStandardItem("")
+                item4 = QStandardItem("")
+
+            model_3.setItem(i, 0, item1)
+            model_3.setItem(i, 1, item2)
+            model_3.setItem(i, 2, item3)
+            model_3.setItem(i, 3, item4)
+    
         self.tableView_3.setModel(model_3)
         self.tableView_3.setColumnWidth(0, 300)
         self.tableView_3.setEditTriggers(QAbstractItemView.NoEditTriggers)
@@ -240,24 +264,41 @@ class Fun(Mainui):
             sheet['A1'] = "UUID"
             sheet['B1'] = "标签"
             sheet['C1'] = "中文标签"
+            sheet['D1'] = "图片名"
 
             sheet.column_dimensions['A'].width = 40
             sheet.column_dimensions['B'].width = 15
+            sheet.column_dimensions['D'].width = 15
 
             for it in self.send_rcc_uuid_list:
-                tmp = it
-                if tmp[-1] == "large_garbage" or tmp[-1] == "garbage":
-                    tmp.append("垃圾")
-                if tmp[-1] == "sewage":
-                    tmp.append("脏污")
-                sheet.append(tmp)
+                sheet.append(it)
 
             sheet2 = workbook.create_sheet("clean result")
             sheet2['A1'] = "UUID"
+            sheet2['B1'] = "标签"
+            sheet2['C1'] = "中文标签"
+            sheet2['D1'] = "图片名"
+
             sheet2.column_dimensions['A'].width = 40
+            sheet2.column_dimensions['B'].width = 15
+            sheet2.column_dimensions['D'].width = 15
 
             for it in self.clean_result_uuid_list:
-                sheet2.append([it])
+                sheet2.append(it)
+            
+            sheet3 = workbook.create_sheet("same")
+
+            sheet3['A1'] = "UUID"
+            sheet3['B1'] = "标签"
+            sheet3['C1'] = "中文标签"
+            sheet3['D1'] = "图片名"
+
+            sheet3.column_dimensions['A'].width = 40
+            sheet3.column_dimensions['B'].width = 15
+            sheet3.column_dimensions['D'].width = 15
+
+            for it in self.same_list:
+                sheet3.append(it)
 
             try:
                 workbook.save(f"{self.input_result.text()}/show.xlsx")
@@ -265,7 +306,7 @@ class Fun(Mainui):
                 yr_btn = reply.addButton(self.tr("确定"), QMessageBox.YesRole)
                 reply.exec_()
             except PermissionError as pe:
-                reply = QMessageBox(QMessageBox.Question, self.tr("警告"), self.tr(pe), QMessageBox.NoButton, self)
+                reply = QMessageBox(QMessageBox.Question, self.tr("警告"), self.tr(str(pe)), QMessageBox.NoButton, self)
                 yr_btn = reply.addButton(self.tr("确定"), QMessageBox.YesRole)
                 reply.exec_()
 
